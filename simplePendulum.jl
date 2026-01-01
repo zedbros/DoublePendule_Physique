eddd2c949c4f774e9086b1daedd4d0942a6132c0
zedbros <jameszed@icloud.com>
2026-01-01 14:36:45 +0100

    Wrong but start of double pendulum (visual the physics are wrong)

--- simplePendulum.jl
+++ simplePendulum.jl
@@ -10,7 +10,7 @@ begin
     
     #////////////L1 section////////////
     global L1 = 0.5                     # [m]
-    global theta1 = [3.0]               # [rad]
+    global theta1 = [3.14/2]               # [rad]
     global angularVelocity1 = [0.0]     # [rad/s]
 
     global posx1 = [L1*sin(theta1[1])]
@@ -21,8 +21,8 @@ begin
     global theta2 = [3.0]               # [rad]
     global angularVelocity2 = [0.0]     # [rad/s]
 
-    global posx2 = [L2*sin(theta2[1])]
-    global posy2 = [-L2*cos(theta2[1])]
+    global posx2 = [L1*sin(theta1[1]) + L2*sin(theta2[1])]
+    global posy2 = [-L1*cos(theta1[1]) - L2*cos(theta2[1])]
 end
 
 #//////////////////////Physics//////////////////////
@@ -36,37 +36,63 @@ begin
         push!(angularVelocity1, new_angularVelocity1)
         push!(posx1, L1*sin(new_theta1))
         push!(posy1, -L1*cos(new_theta1))
+
+        
+        angularAcceleration2 = -(g/L2)*sin(theta2[end])
+        new_angularVelocity2 = angularVelocity2[end] + angularAcceleration2*t
+        new_theta2 = theta2[end] + new_angularVelocity2*t
+
+        push!(theta2, new_theta2)
+        push!(angularVelocity2, new_angularVelocity2)
+        push!(posx2, (L1*sin(new_theta1) + L2*sin(new_theta2)))
+        push!(posy2, (-L1*cos(new_theta1) - L2*cos(new_theta2)))
     end
 end
 
 #//////////////////////Simulation//////////////////////
 begin
     function simulate(step, limitStep)
-        x = Observable(posx1[end])
-        y = Observable(posy1[end])
+        x1 = Observable(posx1[end])
+        y1 = Observable(posy1[end])
+
+        x2 = Observable(posx2[end])
+        y2 = Observable(posy2[end])
 
         global fig = Figure(resolution = (800, 800))
         ax = Axis(
             fig[1, 1],
             aspect = DataAspect(),
-            limits = (-L1-0.1, L1+0.1, -L1-0.1, L1+0.1)
+            limits = (-L1 - L2 - 0.1, L1 + L2 + 0.1, -L1 - L2 - 0.1, L1 + L2 + 0.1) #spaced because posy2 = L1 -L2 somehow causes an error !
         )
 
         #//////Rod build//////
-        rod_points = lift(x, y) do xb, yb
-            Point2f[(0, 0), (xb, yb)]
+        rod1_points = lift(x1, y1) do xb1, yb1
+            Point2f[(0, 0), (xb1, yb1)]
         end
-        lines!(ax, rod_points, linewidth = 3)
+        lines!(ax, rod1_points, linewidth = 1)
+
+        rod2_points = lift(x2, y2) do xb2, yb2
+            Point2f[(posx1[end], posy1[end]), (xb2, yb2)] # There may be an error caused by the starting point since I'm not sure which staged it is at (previous or current).
+        end
+        lines!(ax, rod2_points, linewidth = 1)
+        
 
         #//////Bob build//////
-        scatter!(ax, lift(x, y) do xb, yb
-            Point2f(xb, yb)
-        end, markersize = 20)
+        scatter!(ax, lift(x1, y1) do xb1, yb1
+            Point2f(xb1, yb1)
+        end, markersize = 5)
+
+        scatter!(ax, lift(x2, y2) do xb2, yb2
+            Point2f(xb2, yb2)
+        end, markersize = 3)
 
         #//////Animation loop//////
         for i in 1:step:limitStep
-            x[] = posx1[end]
-            y[] = posy1[end]
+            x1[] = posx1[end]
+            y1[] = posy1[end]
+
+            x2[] = posx2[end]
+            y2[] = posy2[end]
 
             eulerStep(step)
 
