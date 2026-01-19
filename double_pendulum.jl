c488a7654c6e0acffa9289655c75ebce11d29ae2
zedbros <jameszed@icloud.com>
2026-01-19 23:03:45 +0100

    Added energy tests + push before animation rework

--- double_pendulum.jl
+++ double_pendulum.jl
@@ -8,7 +8,7 @@ begin
 
     global g = 9.81                     # [m/s^2]
     
-    #////////////L1 section////////////
+    #/////////////L1 section/////////////
     global m1 = 0.0024                  # [kg]
     global L1 = 0.09174                 # [m]
     global theta1 = [3.15066]           # [rad]
@@ -17,7 +17,7 @@ begin
     global posx1 = [L1*sin(theta1[1])]
     global posy1 = [-L1*cos(theta1[1])]
 
-    #////////////L2 section////////////
+    #/////////////L2 section/////////////
     global m2 = 0.003                   # [kg]
     global L2 = 0.06933                 # [m]
     global theta2 = [3.23707]           # [rad]
@@ -25,6 +25,9 @@ begin
 
     global posx2 = [L1*sin(theta1[1]) + L2*sin(theta2[1])]
     global posy2 = [-L1*cos(theta1[1]) - L2*cos(theta2[1])]
+
+    #///////////Tests section///////////
+    global E = []
 end
 
 #//////////////////////Physics//////////////////////
@@ -39,8 +42,8 @@ begin
         delta = t1 - t2
         den = 2.0*m1+m2-m2*cos(2.0*delta)
 
-        angularAcceleration1 = (-g*(2.0*m1 + m2)*sin(t1) - m2*g*sin(t1 - 2.0*t2) - 2.0*m2*sin(delta)*(w2^2 * L2 + w1^2 * L1*cos(delta))) / (den * L1)
-        angularAcceleration2 = (2.0*sin(delta)*(w1^2 * L1*(m1 + m2) + g*(m1 + m2)*cos(t1) + w2^2 * L2*m2*cos(delta))) / (den * L2)
+        angularAcceleration1 = (-g*(2.0*m1 + m2)*sin(t1) - m2*g*sin(t1 - 2.0*t2) - 2.0*m2*sin(delta)*(w2^2 * L2 + w1^2 * L1*cos(delta))) / (den*L1)
+        angularAcceleration2 = (2.0*sin(delta)*(w1^2 * L1*(m1 + m2) + g*(m1 + m2)*cos(t1) + w2^2 * L2*m2*cos(delta))) / (den*L2)
 
 
         new_angularVelocity1 = w1 + angularAcceleration1*t
@@ -61,6 +64,35 @@ begin
         push!(posy2, (-L1*cos(new_theta1) - L2*cos(new_theta2)))
 
     end
+end
+
+#//////////////////////Tests//////////////////////
+function cin()
+    t1 = theta1[end]
+    t2 = theta2[end]
+    w1 = angularVelocity1[end]
+    w2 = angularVelocity2[end]
+
+    delta = t1-t2
+
+    T1 = 0.5*m1 * (L1*w1)^2
+    T2 = 0.5*m2 * (L1*w1)^2 + (L2*w2)^2 + 2*L1*L2*w1*w2*cos(delta)
+
+    c = T1 + T2
+    return c
+end
+function pot()
+    t1 = theta1[end]
+    t2 = theta2[end]
+
+    y1 = -L1*cos(t1)
+    y2 = y1 - L2*cos(t2)
+
+    p = m1*g*y1 + m2*g*y2
+    return p
+end
+function energy()
+    return cin() + pot()
 end
 
 #//////////////////////Simulation//////////////////////
@@ -101,14 +133,18 @@ begin
         #//////Animation loop//////
         timestamps = range(0.0, limitStep, step = step)
         record(fig, "new_dbl_pen.mp4", timestamps; fps) do t
+            # Positions
             x1[] = posx1[end]
             y1[] = posy1[end]
 
             x2[] = posx2[end]
             y2[] = posy2[end]
 
+            # Tests
+            push!(E, energy())
+
+            # Iterate
             iterate(step)
-            # display(fig)
         end
     end
 end
@@ -117,4 +153,7 @@ end
 #//////////////////////Main//////////////////////
 begin
     simulate(gStep, gLimitStep)
+    print("The variation of energy, throughout this simulation is 
+    start_energy $(E[1]) - end_energy $(E[end]) = $(E[1] - E[end]). 
+    It should be equal to 0, since the double pendulum follows the laws of conservation of energy.")
 end
